bec7bdbbe4dcb78572a3ba07886912201e372127
Francesco Ruggeri <titanfrex@gmail.com>
2023-08-30 15:43:34 +0200

    Initial commit

---
+++ app/Models/Project.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Project extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+
+    protected $fillable = [
+        'title', 'description', 'image', 'status', 'nPartecipants', 'goals', 'budget',
+    ];
+
+    public function type()
+    {
+        return $this->belongsTo(Type::class);
+    }
+
+    public function technologies() {
+        return $this->belongsToMany(Technology::class);
+    }
+}
---
+++ database/migrations/2023_08_28_135612_create_technologies_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('technologies', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 50);
+            $table->string('type', 40);
+            $table->string('color', 30)->nullable()->default('000');;
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('technologies');
+    }
+};
